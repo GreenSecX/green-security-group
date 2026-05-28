d2a977b38b9c052421d4aec154e0ce39c412679b
GreenSecX <paul@greensecuritygroup.com>
2026-05-28 09:11:56 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import PhishingSpotter from './PhishingSpotter.jsx'
+
+const path = window.location.pathname
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {path === '/phishing-spotter' ? <PhishingSpotter /> : <App />}
   </React.StrictMode>
 )
